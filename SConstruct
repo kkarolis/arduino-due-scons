9fa64ad77da2ed043f05d1b006fcfd268d502f68
Kalantojus Karolis <karolis.kalantojus@gmail.com>
2014-10-23 22:33:17 +0300

    Spaghetti implementation of working upload build

--- SConstruct
+++ SConstruct
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim: set filetype=python : #
 from subprocess import call
+from config import *
 
 
 def run_tests(target, source, env):
@@ -8,12 +9,33 @@ def run_tests(target, source, env):
         call(src_f.abspath)
     return None
 
-# Build env
-SRCBUILDDIR = 'build_src'
-TESTBUILDDIR = 'build_test'
-BINDIR = 'bin'
 
-# Mount directories
+env_target = Environment()
+Repository([CORE_SRC, VARIANT_SRC])
+env_target.Append(
+    ARMODEL="Arduino Due",
+    BOSSAC=BOSSAC,
+    BOSSACFLAGS=FLAGS_BOSSAC,
+    ARDUINOPORT=ARDUINO_PORT,
+    MAIN_FILE='build_src/Blink',
+    COREDIR='lib',
+)
+env_target.Replace(
+    AR=AR,
+    OBJCOPY=OBJ_COPY,
+    ARFLAGS='rcs',
+    LIBPREFIX='',
+    CC=CC,
+    CXX=CXX,
+    CCFLAGS=FLAGS_CC,
+    CXXFLAGS=FLAGS_CXX,
+    CPPPATH=incs,
+    FLASHDIR=FLASH,
+    GCCRELDIR=GCC_REL,
+    ELFFLAGS=FLAGS_LINK,
+    OBJCOPYFLAGS=FLAGS_OBJ_COPY,
+)
+# Mount code directories
 Repository(['#./vendor/ceedling/vendor/unity/src'])
 
 CCCOM = '$CC -c $_CCCOMCOM $SOURCE -o $TARGET '
@@ -24,14 +46,27 @@ env = Environment()
 env.Append(
     UNITYHELPDIR=UNITYHELPDIR,
 )
+
 env.Replace(
     CCCOM=CCCOM,
     CPPPATH=CPPPATH,
 )
 
-obj_unity = env.Object('build_src/unity.o', 'unity.c')
-obj_src = SConscript('src/SConscriptHOST', variant_dir=SRCBUILDDIR, duplicate=0,
-                     exports='env')
-test_runners = SConscript('test/SConscript', variant_dir=TESTBUILDDIR,
-                          duplicate=0, exports=['env', 'obj_src', 'obj_unity'])
-env.AlwaysBuild(env.Alias('test', test_runners, run_tests))
+core_variant_syscalls = SConscript('lib/SConscript', duplicate=0,
+                                   exports='env_target')
+bin_main = SConscript('src/SConscriptTARGET', duplicate=0,
+                      variant_dir='build_src', exports=[
+                          'env_target', 'core_variant_syscalls', 'MAIN_FILE'])
+
+env_target.AlwaysBuild(env_target.Alias('compile', bin_main))
+bossacrule = '$BOSSAC $BOSSACFLAGS $SOURCE -R'
+stty_refresh = env_target.AlwaysBuild(env_target.Alias(
+    'stty_refresh',[] ,'stty -F /dev/$ARDUINOPORT cs8 1200 hupcl'))
+env_target.AlwaysBuild(env_target.Alias('upload', [bin_main, stty_refresh], bossacrule))
+
+# obj_unity = env.Object('build_src/unity.o', 'unity.c')
+# obj_src = SConscript('src/SConscriptHOST', variant_dir=SRCBUILDDIR, duplicate=0,
+                     # exports='env')
+# test_runners = SConscript('test/SConscript', variant_dir=TESTBUILDDIR,
+                          # duplicate=0, exports=['env', 'obj_src', 'obj_unity'])
+# env.AlwaysBuild(env.Alias('test', test_runners, run_tests))
--- config.py
+++ config.py
@@ -1,81 +1,59 @@
 # -*- coding: utf-8 -*-
 # Main configuration file for building the project
-main_file = 'Blink.cpp'
-port = 'ttyACM0'
-ar_dir = '/home/karolis/programs/arduino-1.5.8/hardware/'
+import os
 
+MAIN_FILE = 'Blink.cpp'
+ARDUINO_PORT = 'ttyACM0'
+AR_DIR = '/home/karolis/programs/arduino-1.5.8/hardware/'
+
+# Build env
+SRC_BUILD_DIR = 'build_src'
+TEST_BUILD_DIR = 'build_test'
 
 # Should not be modified currently
-env = Environment()
-variant_src_dir = os.path.join(ar_dir, 'arduino/sam/variants/arduino_due_x')
-core_src_dir = os.path.join(ar_dir, 'arduino/sam/cores/arduino')
-tool_dir = os.path.join(ar_dir, 'tools/gcc-arm-none-eabi-4.8.3-2014q1/bin')
-flash_dir = os.path.join(
-    ar_dir, 'arduino/sam/variants/arduino_due_x/linker_scripts/gcc/flash.ld')
-gcc_rel = os.path.join(
-    ar_dir, 'arduino/sam/variants/arduino_due_x/libsam_sam3x8e_gcc_rel.a')
-
-env.Repository([core_src_dir, variant_src_dir])
+VARIANT_SRC = os.path.join(AR_DIR, 'arduino/sam/variants/arduino_due_x')
+CORE_SRC = os.path.join(AR_DIR, 'arduino/sam/cores/arduino')
+TOOLS = os.path.join(AR_DIR, 'tools/gcc-arm-none-eabi-4.8.3-2014q1/bin')
+FLASH = os.path.join(
+    AR_DIR, 'arduino/sam/variants/arduino_due_x/linker_scripts/gcc/flash.ld')
+GCC_REL = os.path.join(
+    AR_DIR, 'arduino/sam/variants/arduino_due_x/libsam_sam3x8e_gcc_rel.a')
+# Mount core repositories
 
 
-inc_dir = os.path.join(ar_dir, 'arduino/sam')
-lib_sam = os.path.join(inc_dir, 'system/libsam')
-lib_cmsis = os.path.join(inc_dir, 'system/CMSIS/CMSIS/Include')
-lib_atmel = os.path.join(inc_dir, 'system/CMSIS/Device/ATMEL')
-lib_arduino = os.path.join(inc_dir, 'cores/arduino')
-lib_arduino_due = os.path.join(inc_dir, 'variants/arduino_due_x')
-incs = [lib_sam, lib_cmsis, lib_atmel, lib_arduino, lib_arduino_due]
+INC_DIR = os.path.join(AR_DIR, 'arduino/sam')
+LIB_SAM = os.path.join(INC_DIR, 'system/libsam')
+LIB_CMSIS = os.path.join(INC_DIR, 'system/CMSIS/CMSIS/Include')
+LIB_ATMEL = os.path.join(INC_DIR, 'system/CMSIS/Device/ATMEL')
+LIB_ARDUINO = os.path.join(INC_DIR, 'cores/arduino')
+LIB_ARDUINO_DUE = os.path.join(INC_DIR, 'variants/arduino_due_x')
+incs = [LIB_SAM, LIB_CMSIS, LIB_ATMEL, LIB_ARDUINO, LIB_ARDUINO_DUE]
 
 
-cc = os.path.join(tool_dir, 'arm-none-eabi-gcc')
-cxx = os.path.join(tool_dir, 'arm-none-eabi-g++')
-ar = os.path.join(tool_dir, 'arm-none-eabi-ar')
-objcopy = os.path.join(tool_dir, 'arm-none-eabi-objcopy')
-bossac = os.path.join(ar_dir, 'tools/bossac')
+CC = os.path.join(TOOLS, 'arm-none-eabi-gcc')
+CXX = os.path.join(TOOLS, 'arm-none-eabi-g++')
+AR = os.path.join(TOOLS, 'arm-none-eabi-ar')
+OBJ_COPY = os.path.join(TOOLS, 'arm-none-eabi-objcopy')
+BOSSAC = os.path.join(AR_DIR, 'tools/bossac')
 
 
 
 
-ccflags = [
+FLAGS_CC = [
     '-g', '-Os', '-w', '-ffunction-sections', '-fdata-sections', '-nostdlib',
     '--param', 'max-inline-insns-single=500', '-Dprintf=iprintf',
     '-mcpu=cortex-m3', '-DF_CPU=84000000L', '-DARDUINO=158',
     '-DARDUINO_SAM_DUE', '-DARDUINO_ARCH_SAM', '-D__SAM3X8E__', '-mthumb',
     '-DUSB_VID=0x2341', '-DUSB_PID=0x003e', '-DUSBCON',
     '-DUSB_MANUFACTURER="Unknown"', '-DUSB_PRODUCT=\"$ARMODEL\"']
-cxxflags = ['-fno-threadsafe-statics', '-fno-rtti', '-fno-exceptions']
-elfflags = ['-Os', '-Wl,--gc-sections', '-mcpu=cortex-m3',
-            '-T$FLASHDIR', '-Wl,-Map,${SOURCE}.map', '-o',
-            '$TARGET', '-L$COREDIR', '-mthumb', '-Wl,--cref',
-            '-Wl,--check-sections', '-Wl,--gc-sections',
-            '-Wl,--entry=Reset_Handler',
-            '-Wl,--unresolved-symbols=report-all', '-Wl,--warn-common',
-            '-Wl,--warn-section-align', '-Wl,--warn-unresolved-symbols',
-            '-Wl,--start-group', '$COREDIR/syscalls_sam3.o',
-            '${SOURCE}', '$COREDIR/variant.o', '$GCCRELDIR',
-            '$COREDIR/core.a', '-Wl,--end-group', '-lm', '-gcc']
-bossac_flags = ['-i', '--port=$ARDUINOPORT', '-U', 'false', '-e', '-w', '-v', '-b']
-objcopyflags = ['-O', 'binary']
-env.Append(
-    ARMODEL="Arduino Due",
-    BOSSAC=bossac,
-    BOSSACFLAGS=bossac_flags,
-    ARDUINOPORT=port,
-)
-env.Replace(
-    AR=ar,
-    OBJCOPY=objcopy,
-    ARFLAGS='rcs',
-    LIBPREFIX='',
-    CC=cc,
-    CXX=cxx,
-    CCFLAGS=ccflags,
-    CXXFLAGS=cxxflags,
-    CPPPATH=incs,
-    FLASHDIR=flash_dir,
-    GCCRELDIR=gcc_rel,
-    COREDIR=core_dir,
-    ELFFLAGS=elfflags,
-    OBJCOPYFLAGS=objcopyflags,
-)
+FLAGS_CXX = ['-fno-threadsafe-statics', '-fno-rtti', '-fno-exceptions']
+FLAGS_LINK = [
+    '-Os', '-Wl,--gc-sections', '-mcpu=cortex-m3', '-T$FLASHDIR', '-lm',
+    '-gcc', '-L$COREDIR', '-mthumb', '-Wl,--cref', '-Wl,--check-sections',
+    '-Wl,--gc-sections', '-Wl,--entry=Reset_Handler',
+    '-Wl,--unresolved-symbols=report-all', '-Wl,--warn-common',
+    '-Wl,--warn-section-align', '-Wl,--warn-unresolved-symbols']
 
+FLAGS_BOSSAC = [
+    '-i', '--port=$ARDUINOPORT', '-U', 'false', '-e', '-w', '-v', '-b']
+FLAGS_OBJ_COPY = ['-O', 'binary']
